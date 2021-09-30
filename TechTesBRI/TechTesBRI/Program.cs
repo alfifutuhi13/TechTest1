70c82ae67c24ea2bfbd8590230635d77d56e413f
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-09-30 08:34:52 +0700

    Initial Push

---
+++ TechTesBRI/TechTesBRI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTesBRI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deret output: 1 4 9 16 25 36
+            Console.WriteLine("Deret: ");
+            for (int i = 1; i<=6; i++)
+            {
+                Console.Write("{0} ",i*i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
